67c9af62f38311593eeb7a54b1f535fc7e864bdc
Alex Turkow <alex_turkow@yaskawa.com>
2018-06-26 14:34:39 -0500

    added DecimalTimeToStringConverter

---
+++ RouteConfigurator/Converters/BaseValueConverter.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Markup;
+
+namespace RouteConfigurator
+{
+    /// <summary>
+    /// A base value converter that allows direct XAML usage
+    /// </summary>
+    /// <typeparam name="T">The type of this value converter</typeparam>
+    public abstract class BaseValueConverter<T> : MarkupExtension, IValueConverter
+        where T : class, new()
+    {
+        #region Private Members
+        private static T mConverter = null;
+        #endregion
+
+        #region Markup Extension Methods
+        /// <summary>
+        /// Provides a static instance of the value converter
+        /// </summary>
+        /// <param name="serviceProvider">The service provider</param>
+        /// <returns></returns>
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            //If(mconverter == null){mConverter = new T()} return mConverter
+            return mConverter ?? (mConverter = new T());
+        }
+        #endregion
+
+        #region Value Converte Methods
+        /// <summary>
+        /// The method to convert one type to another
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="targetType"></param>
+        /// <param name="parameter"></param>
+        /// <param name="culture"></param>
+        /// <returns></returns>
+        public abstract object Convert(object value, Type targetType, object parameter, CultureInfo culture);
+
+        /// <summary>
+        /// The method to convert a value back to it's source type
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="targetType"></param>
+        /// <param name="parameter"></param>
+        /// <param name="culture"></param>
+        /// <returns></returns>
+        public abstract object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture);
+        #endregion
+    }
+}
---
+++ RouteConfigurator/Converters/DecimalTimeToStringConverter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RouteConfigurator
+{
+    class DecimalTimeToStringConverter : BaseValueConverter<DecimalTimeToStringConverter>
+    {
+        public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null)
+                return "0";
+
+            decimal hours = Math.Truncate((decimal)value);
+            decimal minutes = Math.Round(((decimal)value - hours) * 60);
+
+            string averageTimeText = string.Format("{0}:{1:00}", hours, minutes);
+            return averageTimeText;
+
+            throw new NotImplementedException();
+        }
+
+        public override object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
