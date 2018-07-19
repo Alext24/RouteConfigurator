cd8303c3005d9dd2870aba7c07e0f3bb69b79590
Alex Turkow <alex_turkow@yaskawa.com>
2018-07-19 16:22:37 -0500

    Updated a couple little things

--- RouteConfigurator/Converters/DecimalTimeToStringConverter.cs
+++ RouteConfigurator/Converters/DecimalTimeToStringConverter.cs
@@ -17,15 +17,47 @@ namespace RouteConfigurator
             decimal hours = Math.Truncate((decimal)value);
             decimal minutes = Math.Round(((decimal)value - hours) * 60);
 
-            string averageTimeText = string.Format("{0}:{1:00}", hours, minutes);
-            return averageTimeText;
-
-            //throw new NotImplementedException();
+            string timeText = string.Format("{0}:{1:00}", hours, minutes);
+            return timeText;
         }
 
         public override object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            throw new NotImplementedException();
+            if (value == null)
+                return 0;
+
+            string timeText = value.ToString();
+            decimal time = 0;
+
+            // Colon format   12:15
+            if (timeText.Contains(":"))
+            {
+                int colonIndex = timeText.IndexOf(':');
+                int a = colonIndex + 1;
+                int b = timeText.Length - 1 - colonIndex;
+
+                string hoursText = timeText.Substring(0, colonIndex);
+                string minutesText = timeText.Substring(a, b);
+
+                decimal hours = decimal.Parse(hoursText);
+                decimal minutes = decimal.Parse(minutesText);
+
+                time = hours + (minutes / 60);
+            }
+            // Period format   12.25
+            else if (timeText.Contains("."))
+            {
+                try
+                {
+                    time = decimal.Parse(timeText);
+                }
+                catch (Exception)
+                {
+                    time = 0;
+                }
+            }
+
+            return time;
         }
     }
 }
